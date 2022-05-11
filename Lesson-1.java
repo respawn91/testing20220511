f4c9fb10cdbd87ab6f05ae60b4f0fd3735209dd2
Anton Tarasov <respawn91@gmail.com>
2022-05-11 20:36:52 +0300

    Add files via upload

---
+++ Lesson-1.java
@@ -0,0 +1,85 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("Hello World");
+     }
+}
+
+public class HelloWorld{
+
+     public static void main(String []args){
+         
+        int a;
+        a = 10;
+        
+        System.out.println(a);
+        
+        a = 20;
+        System.out.println(a);
+     }
+}
+
+public class HelloWorld{
+
+     public static void main(String []args){
+         
+        int a = 10;
+        double b = 10;
+        String s = "We are going to work hard!";
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+        
+     }
+}
+
+public class HelloWorld{
+
+     public static void main(String []args){
+         
+        int a = 10;
+        int b = 10;
+        
+        int c = a + b;
+        
+        System.out.println(c);
+        
+        int x = 10;
+        double y = 20;
+        
+        double z = x + y;
+        
+        System.out.println(z);
+     }
+}
+
+public class HelloWorld{
+
+     public static void main(String []args){
+         
+        int a = 32;
+        int b = 6;
+        
+        System.out.println(a + b); // 38
+        System.out.println(a - b); // 26
+        System.out.println(a * b); // 192
+        System.out.println(a / b); // 5
+        System.out.println(a % b); // 2
+     }
+}
+
+public class HelloWorld{
+
+     public static void main(String []args){
+         
+        double a = 32;
+        double b = 6;
+        
+        System.out.println(a + b); // 38
+        System.out.println(a - b); // 26
+        System.out.println(a * b); // 192
+        System.out.println(a / b); // 5.(3)
+        System.out.println(a % b); // 2
+     }
+}
